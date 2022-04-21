92eff1a4cbfc63149f77bf97e60db8a265216516
AnmolMaharjan <anmolmhj58@gmail.com>
2022-04-21 17:12:54 +0545

    You need to read again, Susan

---
+++ read_again.py
@@ -0,0 +1,12 @@
+
+# %%
+
+print('''
+aaja bahre haate patuki ko kura gara mayalu
+badhi kammar bhachna mn lagyo
+timro rodhi ghar maruni ko kura gara mayalu
+malai cham cham cham nachna mn lagyo
+a pardeshi tmi sanga chokho saino gasna mn lagyo
+malai cham cham cham nachna mn lagyo''')
+
+# %%
